1c8f830878311d8c20cf6158399ff8d16270a07f
jirichomat <ge0rg3os@gmail.com>
2026-01-27 12:32:28 +0100

    Initial Commit

---
+++ Videographics/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Videographics
+//
+//  Created by Jiri Chomat on 27.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ VideographicsTests/VideographicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  VideographicsTests.swift
+//  VideographicsTests
+//
+//  Created by Jiri Chomat on 27.01.2026.
+//
+
+import Testing
+@testable import Videographics
+
+struct VideographicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
